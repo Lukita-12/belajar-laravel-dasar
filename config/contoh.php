16ff2f125558aef338c954c2d9ddd25b20c5a2c2
Lukita-12 <lukitayapet@gmail.com>
2023-06-14 17:36:43 +0700

    Configuration

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => [
+        "first" => env('NAME_FIRST', 'Yapet'),
+        "last" => env('NAME_LAST', "Lukita")
+    ],
+    "email" => "echo.lukitayapet@gmail.com",
+    "web" => "https://www.lukitayapet.com"
+];
